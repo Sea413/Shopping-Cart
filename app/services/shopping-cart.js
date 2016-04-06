d0695dbe33b89758287bb60942cb13217735d297
Brad Pizzimenti and Sean Peerenboom <seanmp42@gmail.com>
2016-04-06 15:27:11 -0700

    ┓┏┓┏┓┃ ┛┗┛┗┛┃＼○／ ┓┏┓┏┓┃  / ┛┗┛┗┛┃ノ) ┓┏┓┏┓┃ ┛┗┛┗┛┃ ┓┏┓┏┓┃ ┛┗┛┗┛┃ ┓┏┓┏┓┃ ┃┃┃┃┃┃ ┻┻┻┻┻┻

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -3,8 +3,17 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   items: [],
 
+  totalPrice: Ember.computed('items.length', function() {
+    var totalPrice = 0;
+    for (let product of this.get('items')){
+      totalPrice += product.get('price');
+    }
+    return totalPrice;
+
+  }),
+
   add(item) {
     this.get('items').pushObject(item);
-    console.log(this.get("items"));
-  }
+  },
+
 });
